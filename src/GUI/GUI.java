ae6407c23d6b979fd3cf3781b7764f49d9f65c67
JoakimYYY <andaljoakim@gmail.com>
2015-10-11 18:04:13 +0100

    Implemented move and merge methods()

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -13,70 +13,80 @@ import java.util.ArrayList;
 public class GUI extends JFrame implements KeyListener {
 
 
-
     JLabel label[][]; // Grid with GUI of values and color.
     private ArrayList<MyListener> listeners = new ArrayList<>();
-
-
 
 
     public GUI(int x, int y) {
 
 
-            label = new JLabel[x][y];
+        label = new JLabel[x][y];
 
 
-            JPanel grid = new JPanel(new GridLayout(x, y, 2, 2));
-            grid.setFocusable(true);
-            grid.addKeyListener(this);
+        JPanel grid = new JPanel(new GridLayout(x, y, 2, 2));
+        grid.setFocusable(true);
+        grid.addKeyListener(this);
 
-            grid.setBackground( Color.WHITE );
-            grid.setBorder( new MatteBorder(2, 2, 2, 2, Color.BLACK) );
+        grid.setBackground(Color.WHITE);
+        grid.setBorder(new MatteBorder(2, 2, 2, 2, Color.BLACK));
 
-            for (int i = 0; i < x; i++) {
-                for(int j = 0; j<y; j++) {
-                    label[i][j] = new JLabel();
-                    label[i][j].setFont(new Font("Serif", Font.BOLD, 60));
-                    //label[i][j].setFocusable(true);
-                    //label.setPreferredSize(new Dimension(200,200));
-                    //label.setText(" label" + i);
-                    label[i][j].setOpaque(true);
-                    grid.add(label[i][j]);
-                }
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                label[i][j] = new JLabel();
+                label[i][j].setFont(new Font("Serif", Font.BOLD, 60));
+                //label[i][j].setFocusable(true);
+                //label.setPreferredSize(new Dimension(200,200));
+                //label.setText(" label" + i);
+                label[i][j].setOpaque(true);
+                grid.add(label[i][j]);
             }
-
-
-
-
-            add( grid ); // Makes the size niz.
         }
 
 
-    public Color getBackGroundColor(int value){
-        switch (value){
-            case 2:    return new Color(0xeee4da);
-            case 4:    return new Color(0xede0c8);
-            case 8:    return new Color(0xf2b179);
-            case 16:   return new Color(0xf59563);
-            case 32:   return new Color(0xf67c5f);
-            case 64:   return new Color(0xf65e3b);
-            case 128:  return new Color(0xedcf72);
-            case 256:  return new Color(0xedcc61);
-            case 512:  return new Color(0xedc850);
-            case 1024: return new Color(0xedc53f);
-            case 2048: return new Color(0xedc22e);
-            default: return new Color(0,0,0);
+        add(grid); // Makes the size niz.
+    }
+
+
+    public Color getBackGroundColor(int value) {
+        switch (value) {
+            case 0:
+               return UIManager.getColor("Panel.background");
+                //return Color.LIGHT_GRAY;
+            case 2:
+                return new Color(0xeee4da);
+            case 4:
+                return new Color(0xede0c8);
+            case 8:
+                return new Color(0xf2b179);
+            case 16:
+                return new Color(0xf59563);
+            case 32:
+                return new Color(0xf67c5f);
+            case 64:
+                return new Color(0xf65e3b);
+            case 128:
+                return new Color(0xedcf72);
+            case 256:
+                return new Color(0xedcc61);
+            case 512:
+                return new Color(0xedc850);
+            case 1024:
+                return new Color(0xedc53f);
+            case 2048:
+                return new Color(0xedc22e);
+            default:
+                return new Color(0, 0, 0);
 
         }
 
     }
 
 
+    public void setTileSize(int numx, int numy, int Dimx, int Dimy) {
 
-    public void setTileSize(int numx,int numy,int Dimx,int Dimy){
-
-        label[numx-1][numy-1].setPreferredSize(new Dimension(Dimx, Dimy));
+        label[numx - 1][numy - 1].setPreferredSize(new Dimension(Dimx, Dimy));
     }
+
     private void displayInfo(KeyEvent e, String keyStatus) {
 
         //You should only rely on the key char if the event
@@ -96,29 +106,33 @@ public class GUI extends JFrame implements KeyListener {
         System.out.println(keyString);
     }
 
-    public void setTileText(int x,int y, String text){
-        setTileColor(x,y,Integer.parseInt(text));  // setting int and color
-
-        label[x][y].setText(text);
+    public void setTileText(int x, int y, String text) {
+        setTileColor(x, y, Integer.parseInt(text));  // setting int and color
+        if(Integer.parseInt(text) == 0){
+            label[x][y].setText("");
+        }else {
+            label[x][y].setText(text);
+        }
         label[x][y].setHorizontalAlignment(SwingConstants.CENTER);
         label[x][y].setVerticalAlignment(SwingConstants.CENTER);
         label[x][y].setForeground(Color.WHITE);
 
     }
 
-    public void removeTileText(int x, int y){
+    public void removeTileText(int x, int y) {
         label[x][y].setText("");
-        label[x][y].setBackground(new Color(255,255,255));
+        label[x][y].setBackground(UIManager.getColor("Panel.background"));
     }
 
-    public void setTileColor(int x,int y,int number){
+    public void setTileColor(int x, int y, int number) {
         label[x][y].setBackground(getBackGroundColor(number));
     }
 
     public void addListener(MyListener listener) {
         listeners.add(listener);
     }
-    void notifySomethingHappened(){
+
+    void notifySomethingHappened() {
 
     }
 
@@ -130,7 +144,7 @@ public class GUI extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        for(MyListener listener : listeners){   // sends this to interface where a given class is listening.
+        for (MyListener listener : listeners) {   // sends this to interface where a given class is listening.
             listener.keyMovement(e);
         }
     }
@@ -141,9 +155,6 @@ public class GUI extends JFrame implements KeyListener {
     }
 
 }
-
-
-
 
 
 //}
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -9,14 +9,14 @@ https://github.com/bulenkov/2048/blob/master/src/com/bulenkov/game2048/Game2048.
  */
 
 import GUI.GUI;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 import javax.swing.*;
 
 
-public class Game implements MyListener{
+public class Game implements MyListener {
 
 
     private int Score;
@@ -40,11 +40,11 @@ public class Game implements MyListener{
     private int[][] GridValues; // Values
     GUI frame;
 
-    public Game(int x,int y) {
+    public Game(int x, int y) {
         this.width = x;
-        this.height =y;
+        this.height = y;
 
-        frame = new GUI( x, y);
+        frame = new GUI(x, y);
         frame.addListener(this);
 
         this.GridValues = new int[x][y]; // setting size of grid.
@@ -52,60 +52,63 @@ public class Game implements MyListener{
         Score = 0;
         frame.setSize(800, 800);
         frame.setTileSize(x, y, 200, 200);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE );
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setLocationRelativeTo( null );
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         //this.Grid = new int[x][y];
 
 
     }
 
-    public void redraw(int[][] Grid){
+    public void redraw(int[][] Grid) {
 
-        for(int x = 0; x<Grid.length; x++){
-            for (int y = 0; y<Grid[x].length; y++){
-                setTile(x,y,Grid[x][y]);
+        for (int x = 0; x < Grid.length; x++) {
+            for (int y = 0; y < Grid[x].length; y++) {
+                setTile(x, y, Grid[x][y]);
             }
         }
 
 
     }
 
-    public ArrayList getEmptyTiles(){  // getting all the empty tiles
+    public ArrayList getEmptyTiles() {  // getting all the empty tiles
         emptyTiles.clear();
-        for(int x = 0; x < GridValues.length; x++){
-            for(int y=0; y <GridValues[x].length; y++){
-                if(isEmpty(x,y)) emptyTiles.add(i2Dto1D(x,GridValues.length,y));
+        for (int x = 0; x < GridValues.length; x++) {
+            for (int y = 0; y < GridValues[x].length; y++) {
+                if (isEmpty(x, y)) emptyTiles.add(i2Dto1D(x, GridValues.length, y));
             }
         }
         return emptyTiles;
     }
 
-    public int i2Dto1D(int x,int rowlength,int y){
+    public int i2Dto1D(int x, int rowlength, int y) {
         return (x * GridValues.length) + y; // Indexes from 2D to 1D
     }
-    public int[] i1Dto2D(int index){
-        int [] indexes = new int[2];
+
+    public int[] i1Dto2D(int index) {
+        int[] indexes = new int[2];
         indexes[0] = index / width; // X value
         indexes[1] = index % height; // Y value
         return indexes;
     }
 
 
-     public void setTile(int x, int y, int number) {
-        frame.setTileText(x,y,Integer.toString(number));  // color based on number.
-        GridValues[x][y] =number; // setting number to grid
+    public void setTile(int x, int y, int number) {
+
+            frame.setTileText(x, y, Integer.toString(number));  // color based on number.
+
+        GridValues[x][y] = number; // setting number to grid
     }
 
-    public void removeTile(int x, int y){
-        frame.removeTileText(x,y);
+    public void removeTile(int x, int y) {
+        frame.removeTileText(x, y);
         GridValues[x][y] = 0;
     }
 
-    public void chooseEmptySpot(){ // chooses random spot from all the empty tiles.
-       ArrayList arr= getEmptyTiles();
-        if( arr.size() !=0) {
+    public void chooseEmptySpot() { // chooses random spot from all the empty tiles.
+        ArrayList arr = getEmptyTiles();
+        if (arr.size() != 0) {
             int randomIndex = random.nextInt(arr.size()); // index from emptyTiles
             int[] emptyindex = i1Dto2D((int) arr.get(randomIndex));  // index is now X and Y
             setTile(emptyindex[0], emptyindex[1], genTwosfours()); // setting tiles
@@ -113,19 +116,206 @@ public class Game implements MyListener{
 
     }
 
-    public int genTwosfours(){
-        return Math.random() < 0.9 ? 2 : 4; // generating 2s 90 perecent of the time.
+    public boolean mergeTilesUp() {
+        boolean merged = false;
+        int lastValue = -1;
+        int x = -1;
+        int y = -1;
+        for (int i = 0; i < width; i++) {
+            lastValue = -1;
+            x = -1;
+            y = -1;
+            for (int j = 0; j < height; j++) {
+                if (isEmpty(j, i)) {
+
+                } else if (GridValues[j][i] == lastValue) {
+                    setTile(x, y, (lastValue * 2));
+                    removeTile(j, i);
+                    lastValue = 0;
+                    merged = true;
+                }
+                if (isEmpty(j, i)) {
+
+                } else {
+                    lastValue = GridValues[j][i];
+                    x = j;
+                    y = i;
+                }
+            }
+        }
+        return merged;
     }
 
-    public boolean isEmpty(int x,int y){
-        if(GridValues[x][y] ==0) return true;
+    public boolean mergeTilesDown() {
+        boolean merged = false;
+        int lastValue = -1;
+        int x = -1;
+        int y = -1;
+        for (int i = 0; i < width; i++) {
+            lastValue = -1;
+            x = -1;
+            y = -1;
+            for (int j = height -1; j>-1;j--) {
+                if (isEmpty(j, i)) {
+
+                } else if (GridValues[j][i] == lastValue) {
+                    setTile(x, y, (lastValue * 2));
+                    removeTile(j, i);
+                    lastValue = 0;
+                    merged = true;
+                }
+                if (isEmpty(j, i)) {
+
+                } else {
+                    lastValue = GridValues[j][i];
+                    x = j;
+                    y = i;
+                }
+            }
+        }
+        return merged;
+    }
+
+    public boolean mergeTilesLeft() {
+        boolean merged = false;
+        int lastValue = -1;
+        int x = -1;
+        int y = -1;
+        for (int i = 0; i < width; i++) {
+            lastValue = -1;
+            x = -1;
+            y = -1;
+            for (int j = 0 ; j < height;j++) {
+                if (isEmpty(i, j)) {
+
+                } else if (GridValues[i][j] == lastValue) {
+                    setTile(x, y, (lastValue * 2));
+                    removeTile(i, j);
+                    lastValue = 0;
+                    merged = true;
+                }
+                if (isEmpty(i, j)) {
+
+                } else {
+                    lastValue = GridValues[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        return merged;
+    }
+
+    public boolean mergeTilesRight() {
+        boolean merged = false;
+        int lastValue = -1;
+        int x = -1;
+        int y = -1;
+        for (int i = 0; i < width; i++) {
+            lastValue = -1;
+            x = -1;
+            y = -1;
+            for (int j = height-1 ; j > -1;j--) {
+                if (isEmpty(i, j)) {
+
+                } else if (GridValues[i][j] == lastValue) {
+                    setTile(x, y, (lastValue * 2));
+                    removeTile(i, j);
+                    lastValue = 0;
+                    merged = false;
+                }
+                if (isEmpty(i, j)) {
+
+                } else {
+                    lastValue = GridValues[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        return merged;
+    }
+
+    public boolean moveLeft(){
+        for(int i = 0;i<GridValues.length;i++){
+            Integer[] a = new Integer[GridValues[i].length];
+            for(int j = 0;j<GridValues[i].length;j++){
+                a[j] = Integer.valueOf(GridValues[i][j]);
+            }
+
+            Arrays.sort(a, new CompareTiles().LEFT);
+            for(int j = 0;j<GridValues[i].length;j++){
+                setTile(i,j, (int)a[j]);
+            }
+        }
+        return false;
+    }
+    public boolean moveRight(){
+        for(int i = 0;i<GridValues.length;i++){
+            Integer[] a = new Integer[GridValues[i].length];
+            for(int j = 0;j<GridValues[i].length;j++){
+                a[j] = Integer.valueOf(GridValues[i][j]);
+            }
+
+            Arrays.sort(a, new CompareTiles().RIGHT);
+            for(int j = 0;j<GridValues[i].length;j++){
+                setTile(i,j, (int)a[j]);
+            }
+        }
         return false;
     }
 
-    public boolean wonGame(){
-        for(int i=0; i<GridValues.length; i++){
-            for(int j=0; j<GridValues[i].length; j++){
-                if(GridValues[i][j]==GOAL) return true; // Checking for 2048
+    public boolean moveUp(){
+        for(int i = 0;i<GridValues.length;i++){
+            Integer[] a = new Integer[GridValues[i].length];
+            for(int j = 0;j<GridValues[i].length;j++){
+                a[j] = Integer.valueOf(GridValues[j][i]);
+            }
+
+            Arrays.sort(a, new CompareTiles().UP);
+            for(int j = 0;j<GridValues[i].length;j++){
+                setTile(j,i, (int)a[j]);
+            }
+        }
+        return false;
+    }
+    public boolean moveDown(){
+        for(int i = 0;i<GridValues.length;i++){
+            Integer[] a = new Integer[GridValues[i].length];
+            for(int j = 0;j<GridValues[i].length;j++){
+                a[j] = Integer.valueOf(GridValues[j][i]);
+            }
+
+            Arrays.sort(a, new CompareTiles().DOWN);
+            for(int j = 0;j<GridValues[i].length;j++){
+                setTile(j,i, (int)a[j]);
+            }
+        }
+        return false;
+    }
+
+
+
+    public boolean compare(int a, int b) {
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+
+    public int genTwosfours() {
+        return Math.random() < 0.9 ? 2 : 4; // generating 2s 90 perecent of the time.
+    }
+
+    public boolean isEmpty(int x, int y) {
+        if (GridValues[x][y] == 0) return true;
+        return false;
+    }
+
+    public boolean wonGame() {
+        for (int i = 0; i < GridValues.length; i++) {
+            for (int j = 0; j < GridValues[i].length; j++) {
+                if (GridValues[i][j] == GOAL) return true; // Checking for 2048
             }
         }
         return false;
@@ -133,32 +323,92 @@ public class Game implements MyListener{
 
     @Override
     public void keyMovement(KeyEvent e) {  // The callback is processed here. The merging can begin.
-        switch (e.getKeyCode()){
+        switch (e.getKeyCode()) {
 
             case UP: // UPs
                 System.out.println("UP");
+                mergeTilesUp();
+                moveUp();
                 chooseEmptySpot();
 
                 break;
 
             case DOWN: // DOWN
                 System.out.println("DOWN");
+                mergeTilesDown();
+                moveDown();
                 chooseEmptySpot();
                 break;
 
             case LEFT: // LEFT
                 System.out.println("LEFT");
+                mergeTilesLeft();
+                moveLeft();
                 chooseEmptySpot();
                 break;
 
             case RIGHT: // RIGHT
                 System.out.println("RIGHT");
+                mergeTilesRight();
+                moveRight();
                 chooseEmptySpot();
                 break;
 
             default:
                 System.out.println("WRONG FUCKING KEY");
         }
+    }
+
+    public class CompareTiles{
+        public final Comparator<Integer> LEFT = new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                if(a == 0 && b != 0){
+                    return 1;
+                }else if(a != 0 && b == 0){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+        };
+        public  final Comparator<Integer> RIGHT = new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                if(a == 0 && b != 0){
+                    return -1;
+                }else if(a != 0 && b == 0){
+                    return 1;
+                }else{
+                    return 0;
+                }
+            }
+        };
+        public  final Comparator<Integer> UP = new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                if(a == 0 && b != 0){
+                    return 1;
+                }else if(a != 0 && b == 0){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+        };
+        public  final Comparator<Integer> DOWN = new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                if(a == 0 && b != 0){
+                    return -1;
+                }else if(a != 0 && b == 0){
+                    return 1;
+                }else{
+                    return 0;
+                }
+            }
+        };
+
     }
 }
 
