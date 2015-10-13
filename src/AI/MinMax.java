e4277be47860780654325123591920c3ca008280
bjornamr <bjornarremm@yahoo.no>
2015-10-13 16:52:46 +0200

    Minmax class

--- src/AI/MinMax.java
+++ src/AI/MinMax.java
@@ -3,25 +3,40 @@ package AI;
 /**
  * Created by Bjornars on 13.10.2015.
  */
+
 public class MinMax {
 
 
-    function alphabeta(node, depth, ?, ?, maximizingPlayer)
-    if depth = 0 or node is a terminal node
-    return the heuristic value of node
-    if maximizingPlayer
-    for each child of node
-    ? := max(?, alphabeta(child, depth - 1, ?, ?, FALSE))
-            if ? ? ?
-    break (* ? cut-off *)
-            return ?
-    else
-            for each child of node
-    ? := min(?, alphabeta(child, depth - 1, ?, ?, TRUE))
-            if ? ? ?
-    break (* ? cut-off *)
-            return ?
-            (* Initial call *)
-    alphabeta(origin, depth, -?, +?, TRUE)
 
+
+
+    public int minMax(SearchNode node, int depth, boolean maximizing){
+        if( depth =0 || node.isTerminal()){
+            return node.getHeuristicValue();
+        }
+        if (maximizing){
+            int bestValue = 999999999;
+            SearchNode[] children = node.getChildren();
+            for(SearchNode child: getMaxChildren()){
+                bestValue = Math.max(bestValue, minMax(child, depth - 1, false));
+
+            }
+            return bestValue;
+        }else{
+            int bestValue = 999999999;
+            SearchNode[] children = node.getChildren();
+            for(SearchNode child: getMinChildren()){
+                bestValue = Math.min(bestValue, minMax(child,depth-1,true));
+
+            }
+            return bestValue;
+        }
+        return -1;
+    }
+
+
+    public  SearchNode[] getMinChildren(int[][] State){
+
+
+    }
 }
